9a1171ad960f4f81ea36a5dda19d97b276a1d3f0
Kuniaki IGARASHI <igaiga@gmail.com>
2017-01-08 18:42:53 +0900

    Update books_app to Rails.5.0.1 with Ruby2.4.0

--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,15 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161030225951) do
+ActiveRecord::Schema.define(version: 20170108085524) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.text     "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "author"
-    t.string   "picture"
   end
 
 end
