38dee3e1eb96656bede029b9052279a99e988cbb
Kuniaki IGARASHI <igaiga@gmail.com>
2017-06-27 18:33:43 +0900

    Upgrade books_app to Rails5.1.2

--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170605002733) do
+ActiveRecord::Schema.define(version: 20170627092715) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
