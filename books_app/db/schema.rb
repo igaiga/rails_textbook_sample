7f085b220070f2004c9beffe3457a19f4039dec4
Kuniaki IGARASHI <igaiga@gmail.com>
2017-06-29 09:08:22 +0900

    create books_app in Rails5.1.2

--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628235843) do
+ActiveRecord::Schema.define(version: 20170628235935) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170628235843) do
     t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
 end
