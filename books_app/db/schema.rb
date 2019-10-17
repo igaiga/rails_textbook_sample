7224a664d498a76ffcc18bb60ab70404c0e6b2f5
Kuniaki Igarashi <igaiga@gmail.com>
2019-10-18 08:14:32 +0900

    Add Book#author in books_app

--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_234834) do
+ActiveRecord::Schema.define(version: 2019_10_17_231132) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "author"
   end
 
 end
