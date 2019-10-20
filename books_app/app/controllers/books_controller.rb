edcfc99a9bfe9276b2d7dd3e1e7854a7a81d8d0e
Kuniaki Igarashi <igaiga@gmail.com>
2019-10-21 08:37:28 +0900

    Add carrierwave to books_app

--- books_app/app/controllers/books_controller.rb
+++ books_app/app/controllers/books_controller.rb
@@ -69,6 +69,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :memo, :author)
+      params.require(:book).permit(:title, :memo, :author, :picture)
     end
 end
--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_17_231132) do
+ActiveRecord::Schema.define(version: 2019_10_20_225655) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_10_17_231132) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "author"
+    t.string "picture"
   end
 
 end
