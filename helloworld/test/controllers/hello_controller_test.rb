a2212384be52ac3a9eb628b8251dbb4dbfd76af1
Kuniaki Igarashi <igaiga@gmail.com>
2022-01-11 08:44:24 +0900

    Update helloworld for Ruby3.1.0 and Rails7.0.1

--- helloworld/test/controllers/hello_controller_test.rb
+++ helloworld/test/controllers/hello_controller_test.rb
@@ -1,9 +1,8 @@
-require 'test_helper'
+require "test_helper"
 
 class HelloControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get hello_index_url
     assert_response :success
   end
-
 end
