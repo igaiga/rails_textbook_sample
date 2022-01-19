cbb69f1699c8fa0a7defbbd04eac5887a029f950
Kuniaki Igarashi <igaiga@gmail.com>
2022-01-19 10:14:45 +0900

    Fix problem for db migrations for books_app

--- books_app/db/schema.rb
+++ books_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_10_235334) do
+ActiveRecord::Schema.define(version: 2022_01_10_235350) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
